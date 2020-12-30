6389190942111c00755a541ae4ccde715719a264
william koller <wkoller25@gmail.com>
2020-12-30 18:40:08 -0300

    feat: add ContinentsService w create

---
+++ src/continents/services/services.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common'
+import { CreateContinentDto } from '../dtos/create-continent.dto'
+import { ContinentsRepository } from '../repositories/continents.repository'
+import { Continent } from '../schemas/continents.schema'
+
+@Injectable()
+export class ContinentsService {
+  constructor(private readonly continentsRepository: ContinentsRepository) {}
+
+  async create(data: CreateContinentDto): Promise<Continent> {
+    return await this.continentsRepository.create(data)
+  }
+}
