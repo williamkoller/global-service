698ce2856c31cb89d5154c0932f2b65a5f793b1c
william koller <wkoller25@gmail.com>
2020-12-30 18:39:54 -0300

    feat: add GraphQLModule and MongooseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
 import { MongooseModule } from '@nestjs/mongoose'
+import { ContinentsModule } from './continents/continents.module'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
+      isGlobal: true,
     }),
-    MongooseModule.forRoot(`${process.env.MONGO_URI}`),
+    GraphQLModule.forRoot({
+      include: [ContinentsModule],
+    }),
+    MongooseModule.forRoot(`${process.env.MONGO_URI}`, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }),
+    ContinentsModule,
   ],
   controllers: [],
   providers: [],
