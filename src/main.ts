a8bda1f662e6271495aaf05249e56113a172f740
william koller <wkoller25@gmail.com>
2020-12-30 21:55:55 -0300

    feat: add ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import 'module-alias/register'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(new ValidationPipe())
   await app.listen(process.env.PORT)
 }
 bootstrap()
