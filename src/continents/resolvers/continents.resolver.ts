03f4135ddd827b068f785304300072ca9f2b19a8
william koller <wkoller25@gmail.com>
2021-01-01 04:31:29 -0300

    feat: add updaqte continent on resolver

--- src/continents/resolvers/continents.resolver.ts
+++ src/continents/resolvers/continents.resolver.ts
@@ -1,5 +1,6 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
 import { CreateContinentDto } from '../dtos/create-continent.dto'
+import { UpdateContinentDto } from '../dtos/update-continent.dto'
 import { Continent } from '../schemas/continents.schema'
 import { ContinentsService } from '../services/services.service'
 
@@ -25,5 +26,10 @@ export class ContinentsResolver {
   @Query(() => [Continent])
   async findByName(@Args('name') name: string): Promise<Array<Continent>> {
     return await this.continentsService.findByName(name)
+  }
+
+  @Mutation(() => Continent)
+  async update(@Args('_id') _id: string, @Args('data') data: UpdateContinentDto): Promise<Continent> {
+    return await this.continentsService.update(_id, data)
   }
 }
