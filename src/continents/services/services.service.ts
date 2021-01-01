d655a4371c9f3b5aff8c88968e999b64fc566daa
william koller <wkoller25@gmail.com>
2021-01-01 04:31:13 -0300

    feat: add update

--- src/continents/services/services.service.ts
+++ src/continents/services/services.service.ts
@@ -1,5 +1,6 @@
 import { BadRequestException, Injectable } from '@nestjs/common'
 import { CreateContinentDto } from '../dtos/create-continent.dto'
+import { UpdateContinentDto } from '../dtos/update-continent.dto'
 import { ContinentsRepository } from '../repositories/continents.repository'
 import { Continent } from '../schemas/continents.schema'
 
@@ -23,5 +24,14 @@ export class ContinentsService {
     const continentFound = await this.continentsRepository.findByName(name)
     if (!continentFound) throw new BadRequestException('No results for this name.')
     return continentFound
+  }
+
+  async update(_id: string, data: UpdateContinentDto): Promise<Continent> {
+    const updateContinent = await this.continentsRepository.update(_id, data)
+    if (updateContinent) {
+      return this.continentsRepository.findById(_id)
+    } else {
+      throw new BadRequestException('Error in update continent')
+    }
   }
 }
