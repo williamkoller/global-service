481e30225751ecb528c7a43155f03752c9937803
william koller <wkoller25@gmail.com>
2021-01-01 01:09:58 -0300

    fix: remove LoggingInterceptor on controller

--- src/continents/controllers/continents.controller.ts
+++ src/continents/controllers/continents.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, Get, UseInterceptors } from '@nestjs/common'
-import { LoggingInterceptor } from 'src/interceptors/logging.interceptor'
 import { TransformInterceptor } from 'src/interceptors/transform.interceptor'
 import { Continent } from '../schemas/continents.schema'
 import { ContinentsService } from '../services/services.service'
@@ -9,7 +8,7 @@ export class ContinentsController {
   constructor(private readonly continentsService: ContinentsService) {}
 
   @Get()
-  @UseInterceptors(TransformInterceptor, LoggingInterceptor)
+  @UseInterceptors(TransformInterceptor)
   async findAll(): Promise<Array<Continent>> {
     return await this.continentsService.findAll()
   }
