e18af2ca5a7b5017f4aa1175a4bec9b19519ed76
william koller <wkoller25@gmail.com>
2021-01-03 22:45:54 -0300

    feat: add HttpExceptionFilter

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 import { LoggingInterceptor } from './interceptors/logging.interceptor'
+import { HttpExceptionFilter } from './common/filters/http-exception.filter'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.useGlobalFilters(new HttpExceptionFilter())
   app.useGlobalInterceptors(new LoggingInterceptor())
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(process.env.PORT)
